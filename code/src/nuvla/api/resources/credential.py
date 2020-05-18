39b28aae8b49574f89880fc6a8bc3065dee677aa
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2020-05-18 13:45:34 +0200

    creds for k8s infra service

--- code/src/nuvla/api/resources/credential.py
+++ code/src/nuvla/api/resources/credential.py
@@ -1,7 +1,12 @@
 
-class Credential(object):
+from nuvla.api import Api as Nuvla
 
-    def __init__(self, nuvla):
+
+class Credential:
+
+    resource = 'credential'
+
+    def __init__(self, nuvla: Nuvla):
         self.nuvla = nuvla
 
     def find(self, infra_service_id):
@@ -11,7 +16,23 @@ class Credential(object):
         :return: list
         """
         filters = "parent='{}'".format(infra_service_id)
-        creds = self.nuvla.search('credential', filter=filters, select="id")
+        creds = self.nuvla.search(self.resource, filter=filters, select="id")
         return creds.resources
 
 
+class CredentialK8s(Credential):
+
+    def add(self, ca, cert, key, infra_service_id, name, description=None):
+        iscred_k8s = {
+            "name": name,
+            "description": description or name,
+            "template": {
+                "href": "credential-template/infrastructure-service-swarm",
+                "parent": infra_service_id,
+                "ca": ca,
+                "cert": cert,
+                "key": key
+            }
+        }
+        res = self.nuvla.add(self.resource, iscred_k8s)
+        return res.data['resource-id']
