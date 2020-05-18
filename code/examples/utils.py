6205500755ae3213ad4c22ddbf83fde39b453645
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2020-05-18 13:46:25 +0200

    minor

--- code/examples/utils.py
+++ code/examples/utils.py
@@ -7,7 +7,7 @@ CONFIG = 'nuvla.conf'
 def nuvla_conf_user_pass(fn=CONFIG):
     config = RawConfigParser()
     if fn not in config.read(fn):
-        print("Failed reading config file {0}".format(cf))
+        print(f'Failed reading config file {fn}')
         exit(1)
     username = config['default'].get('username')
     password = config['default'].get('password')
