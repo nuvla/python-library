f2fd42dd32e4298aa391166bb11a5794f88f6a3f
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2022-03-30 15:24:28 +0200

    make sonar happier

--- code/tests/api/util/filter_test.py
+++ code/tests/api/util/filter_test.py
@@ -5,6 +5,10 @@ from nuvla.api.util.filter import filter_or, filter_and
 
 class TestSearchUtil(unittest.TestCase):
 
+    attr_a = 'attr="a"'
+    attr_b = 'attr="b"'
+    attr_not_b = 'attr!="b"'
+
     def test_filter_or(self):
         self.assertEqual('', filter_or([]))
         self.assertEqual('', filter_or([None]))
@@ -12,8 +16,8 @@ class TestSearchUtil(unittest.TestCase):
         self.assertEqual('a', filter_or(['a', '']))
         self.assertEqual('(a or b)', filter_or(['a', 'b']))
         self.assertEqual('attr^="hello"', filter_or(['attr^="hello"']))
-        self.assertEqual('(attr="a" or attr!="b")',
-                         filter_or(['attr="a"', 'attr!="b"']))
+        self.assertEqual(f'({self.attr_a} or {self.attr_not_b})',
+                         filter_or([self.attr_a, self.attr_not_b]))
 
     def test_filter_and(self):
         self.assertEqual('', filter_and([]))
@@ -22,21 +26,20 @@ class TestSearchUtil(unittest.TestCase):
         self.assertEqual('', filter_and([None, None]))
         self.assertEqual('a', filter_and(['a', '']))
         self.assertEqual('(a and b)', filter_and(['a', 'b']))
-        self.assertEqual('(attr="a" and attr!="b")',
-                         filter_and(['attr="a"', 'attr!="b"']))
-        self.assertEqual('(attr="a" and attr!="b")',
-                         filter_and(['attr="a"', 'attr!="b"']))
+        self.assertEqual('attr^="hello"', filter_and(['attr^="hello"']))
+        self.assertEqual(f'({self.attr_a} and {self.attr_not_b})',
+                         filter_and([self.attr_a, self.attr_not_b]))
 
     def test_filter_combination(self):
         self.assertEqual('', filter_and([filter_or([])]))
-        self.assertEqual('attr="a"', filter_and([filter_or(['attr="a"'])]))
-        self.assertEqual('(attr1="a" or attr2="b")',
+        self.assertEqual(self.attr_a, filter_and([filter_or([self.attr_a])]))
+        self.assertEqual(f'({self.attr_a} or {self.attr_b})',
                          filter_and(
-                             [filter_or(['attr1="a"', 'attr2="b"'])]))
-        self.assertEqual('(attr1="a" or attr2="b")',
+                             [filter_or([self.attr_a, self.attr_b])]))
+        self.assertEqual(f'({self.attr_a} or {self.attr_b})',
                          filter_and(
-                             [filter_or(['attr1="a"', 'attr2="b"']), None]))
-        self.assertEqual('((attr1="a" or attr2="b") and attr2="c")',
+                             [filter_or([self.attr_a, self.attr_b]), None]))
+        self.assertEqual(f'(({self.attr_a} or {self.attr_b}) and attr2="c")',
                          filter_and(
-                             [filter_or(['attr1="a"', 'attr2="b"']),
+                             [filter_or([self.attr_a, self.attr_b]),
                               'attr2="c"']))
