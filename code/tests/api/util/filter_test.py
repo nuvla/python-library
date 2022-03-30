cbb9e6245f207d434e748ec1c9c8058c4e9bda20
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2022-03-30 15:27:02 +0200

    make sonar even more happier

--- code/tests/api/util/filter_test.py
+++ code/tests/api/util/filter_test.py
@@ -8,6 +8,7 @@ class TestSearchUtil(unittest.TestCase):
     attr_a = 'attr="a"'
     attr_b = 'attr="b"'
     attr_not_b = 'attr!="b"'
+    attr_hello = 'attr^="hello"'
 
     def test_filter_or(self):
         self.assertEqual('', filter_or([]))
@@ -15,7 +16,7 @@ class TestSearchUtil(unittest.TestCase):
         self.assertEqual('', filter_or([None, None]))
         self.assertEqual('a', filter_or(['a', '']))
         self.assertEqual('(a or b)', filter_or(['a', 'b']))
-        self.assertEqual('attr^="hello"', filter_or(['attr^="hello"']))
+        self.assertEqual(self.attr_hello, filter_or([self.attr_hello]))
         self.assertEqual(f'({self.attr_a} or {self.attr_not_b})',
                          filter_or([self.attr_a, self.attr_not_b]))
 
@@ -26,7 +27,7 @@ class TestSearchUtil(unittest.TestCase):
         self.assertEqual('', filter_and([None, None]))
         self.assertEqual('a', filter_and(['a', '']))
         self.assertEqual('(a and b)', filter_and(['a', 'b']))
-        self.assertEqual('attr^="hello"', filter_and(['attr^="hello"']))
+        self.assertEqual(self.attr_hello, filter_and([self.attr_hello]))
         self.assertEqual(f'({self.attr_a} and {self.attr_not_b})',
                          filter_and([self.attr_a, self.attr_not_b]))
 
