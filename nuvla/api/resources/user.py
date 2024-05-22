049e31a3ee89b278a5b7f0b98b8ffbac2133e583
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2024-05-22 22:05:20 +0200

    add user login with api key and refactor

--- nuvla/api/resources/user.py
+++ nuvla/api/resources/user.py
@@ -1,8 +1,31 @@
 
+from requests import Response
 from requests.exceptions import HTTPError
 
 from .base import ResourceBase
 from ..api import NuvlaError
+
+
+def handle_HTTPErrorException(e: HTTPError, response: Response):
+    try:
+        json_msg = e.response.json()
+        message = json_msg.get('message')
+        if message is None:
+            error = json_msg.get('error')
+            message = error.get('code') + ' - ' + error.get('reason')
+    except:
+        try:
+            message = e.response.text
+        except:
+            message = str(e)
+    raise NuvlaError(message, response)
+
+
+def handle_response(response: Response):
+    try:
+        response.raise_for_status()
+    except HTTPError as e:
+        handle_HTTPErrorException(e, response)
 
 
 class User(ResourceBase):
@@ -19,21 +42,15 @@ class User(ResourceBase):
 
     def login_password(self, username, password):
         response = self.nuvla.login_password(username, password)
-        try:
-            response.raise_for_status()
-        except HTTPError as e:
-            try:
-                json_msg = e.response.json()
-                message = json_msg.get('message')
-                if message is None:
-                    error = json_msg.get('error')
-                    message = error.get('code') + ' - ' + error.get('reason')
-            except:
-                try:
-                    message = e.response.text
-                except:
-                    message = str(e)
-            raise NuvlaError(message, response)
+
+        handle_response(response)
+
+        return response.json().get('resource-id')
+
+    def login_apikey(self, key, secret):
+        response = self.nuvla.login_apikey(key, secret)
+
+        handle_response(response)
 
         return response.json().get('resource-id')
 
