c7ac1b94d3993484cfff1eff5f8f3ab4fe18d3f5
giovannibianco <john.white@sixsq.com>
2024-06-17 14:49:45 +0200

    style: add more comments

--- examples/create-data-object.py
+++ examples/create-data-object.py
@@ -30,10 +30,12 @@ user_api = User(nuvla)
 user_api.login(username, password)
 
 # REQUIRED: ID of S3 credential in Nuvla.
+# either we already know this and it is hardcoded below
 # s3_cred_id = 'credential/362e6088-394d-4de9-9667-8e1a40136aa6'
 # s3_cred_id = 'credential/0f70f0fb-c226-4fc2-a98b-61f0646813d7'
 
 # OR we can get the credentials from a login if we want...
+# as in below
 creds = nuvla.get('credential')
 s3_cred_id = None
 for cred in creds.data['resources']:
@@ -44,7 +46,7 @@ for cred in creds.data['resources']:
             print(f"Found Exoscale S3 credential: {s3_cred_id}")
     except (NuvlaError, KeyError):
         pass
-# exit(1)
+# if we didn't find the credential, we should exit
 if not s3_cred_id:
     print("No Exoscale S3 credential found. Exiting.")
     exit(1)
@@ -52,12 +54,11 @@ if not s3_cred_id:
 #
 # Set correct values for variable below.
 #
-
-
 # Bucket to store the test objects.
-bucket = 'bucket-for-extract-project-demo-purposes-001'
+# bucket will be created if it doesn't exist?
+#
+bucket = 'bucket-for-extract-project-demo-purposes-002'
 # can we get the bucket name from the credential?
-
 
 #
 # data-object API object.
@@ -68,40 +69,57 @@ data_obj_api = DataObjectS3(nuvla)
 # Create binary object on S3 and register it in Nuvla.
 #
 print("\nCreating binary object\n")
-# here is the file that acutally gets uploaded to S3
+# here is the file that actually gets uploaded to S3
 content = open('data/african-lion.jpg', 'rb').read()
 # here is the path where the file will be stored in S3
 object_path = 'africa/african-lion3.jpg'
 # here is the content type of the file
 content_type = 'image/scary-lions'
 
-def generate_event(local_event_record) -> str:
+def generate_event(local_event_record: dict) -> str:
+    '''
+    Generate an event in Nuvla
+    :param local_event_record: dict - event record
+    :return: str - event id
+    '''
     try:
-        local_event_record = nuvla.add('event', local_event_record)
-    except NuvlaError:
-        print('Failed to create event.')
-    
-    return local_event_record
+        cimi_event = nuvla.add('event', local_event_record)
+    except NuvlaError as e:
+        print(f'Failed to create event: {e}')
+        return None
+    event_id = cimi_event.data['resource-id']
+    print('created event:', event_id, "with data:")
+    pp(cimi_event.data)
+    return event_id
 
-# should generate an event here as a data record was created
+# should generate an event here as a data record was create
 # maybe this should be combined?
 
-def create_data_record(local_data_record) -> str:
+def create_data_record(local_data_record: dict) -> str:
+    '''
+    Create a data record in Nuvla
+    :param local_data_record: dict - data record
+    :return: str - data record id
+    if the data record already exists, it will be deleted and recreated
+    possibly should be a recursive function
+    '''
     try:
-        dr_id = dr_api.add(local_data_record)
+        dr_id_created = dr_api.add(local_data_record)
     except NuvlaError as e:
         for arg in e.args:
             if 'data-record' in arg:
-                dr_id = arg.split(' ')[-1]
-                print(f'Data record already exists, data record id: {dr_id}. Deleting and recreating.')
-                deleted_dr_id = dr_api.delete(dr_id)
-                print('Deleted data record id:', deleted_dr_id)
-                local_data_record = create_data_record(local_data_record)
-                break
+                dr_id_created = arg.split(' ')[-1]
+                print(f'Data record already exists, data record id: {dr_id_created}. Deleting and recreating.')
+                deleted_dr_id = dr_api.delete(dr_id_created)
+                if deleted_dr_id:
+                    print('Deleted data record id:', deleted_dr_id)
+                    dr_id_created = create_data_record(local_data_record)
+                    break
         
-        return dr_id
+        return dr_id_created
 
-    return dr_id    
+    return dr_id_created
+
 def create_data_object(local_content, local_bucket: str, local_object_path: str, local_s3_cred_id: str, local_content_type: str, local_tags) -> str:
     try:
         object_id = data_obj_api.create(local_content, local_bucket, local_object_path, local_s3_cred_id,
@@ -113,27 +131,31 @@ def create_data_object(local_content, local_bucket: str, local_object_path: str,
                 object_id = arg.split(' ')[-1]
                 print(f'Object already exists, object id: {object_id}. Deleting and recreating.')
                 deleted_object_id = data_obj_api.delete(object_id)
-                print('Deleted object id:', deleted_object_id)
-                object_id = create_data_object(local_content, local_bucket, local_object_path, local_s3_cred_id, local_content_type, local_tags)
-                break
+                if deleted_object_id:
+                    print('Deleted object id:', deleted_object_id)
+                    object_id = create_data_object(local_content, local_bucket, local_object_path, local_s3_cred_id, local_content_type, local_tags)
+                    break
         return object_id
     return object_id
 
 # Add binary object.
 # Bucket will be created if it doesn't exist.
+# tags can be added to the object
 tags = ["zoo", "africa", "lion"]
 bin_object_id = create_data_object(content, bucket, \
                               object_path, s3_cred_id, \
                               content_type, tags)
 # Get object document.
+# print out the object document
 obj_doc = data_obj_api.get(bin_object_id)
 pp(obj_doc)
 
 # create a data record for the object
 dr_api = DataRecord(nuvla)
+# find the S3 infrastructure service
+# either it's known and hardcoded or we can search for it using the credential
 # s3_infra_service_id = 'infrastructure-service/09b08b49-2408-4b80-a7cc-73f420903fd5'
 cred_doc = nuvla.get(s3_cred_id) 
-print('credential doc:', cred_doc.data['parent'])
 s3_infra_service_id=cred_doc.data['parent']
 if not s3_infra_service_id:
     print('No infrastructure service found for the credential. Exiting.')
@@ -168,15 +190,16 @@ event_record = {
         "content": {
           "content-type": content_type,
         },
-        "state": "created",
-      }
+      },
+      "state": "created",
     },
     "severity": "medium",
     "tags": data_record['tags'],
 }
 
 event = generate_event(event_record)
-print('created event:', event)
+event_structure = nuvla.get(event).data
+pp(event_structure)
 
 # Download object and store it locally to a file.
 local_filename = './data/local-african-lion.jpg'
