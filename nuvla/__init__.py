18e3116af7ccb3ddf04984b4ae1003f22597a382
schaubl <schaubl@users.noreply.github.com>
2024-10-02 21:27:31 +0200

    feat: add support for Python 3.12 refactor: improved creation of namespace package deps: updated indirect dependencies

--- nuvla/__init__.py
+++ nuvla/__init__.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
-__import__("pkg_resources").declare_namespace(__name__)
+import logging
+
+try:
+    __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+except Exception as e:
+    logging.debug(f'Failed to create namespace package with pkgutil.extend_path: {e}')
+    try:
+        __import__('pkg_resources').declare_namespace(__name__)
+    except Exception as ex:
+        logging.debug(f'Failed to create namespace package with pkg_resources.declare_namespace: {ex}')
